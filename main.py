43ac2f4631a759d68a7c69d716e7519a2f41feee
Peter Schulman <pks5481@psu.edu>
2020-09-10 14:16:29 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+# jiulong Tang jzt5526@psu.edu
+ # Brendan Corso bvc5434@bvc.edu
+ # Mack Mason mjm8542@p
+
+lettergrade = "no"
+def getLetterGrade(grade):
+  if grade <= 100 and grade >= 93:
+    return "A"
+  elif grade < 93 and grade >= 90:
+    return "A-"
+  elif grade < 90 and grade >= 87:
+    return "B+"
+  elif grade < 87 and grade >= 83:
+    return "B"
+  elif grade < 83 and grade >= 80:
+    return "B-"
+  elif grade < 80 and grade >= 77:
+    return = "C+"
+  elif grade <= 77 and grade >= 70:
+    return "C"
+  elif grade < 70 and grade >= 60:
+    return "D"
+  else
+    return "F"
+  print(f")
+  
+def run():
+  grade = float(input("Enter your CMPSC 131 grade: "))
+  print(f"your letter grade for CMPSC 131 is {lettergrade(grade)}.")
+
+if __name__ == "__main__":
+  run()
